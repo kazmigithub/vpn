d8901ca0b4b4c8ceca7e540bd335d7262e346a7a
kazmigithub <khairulklang@gmail.com>
2016-08-03 14:54:14 +0800

    Update centos6.sh

--- centos6.sh
+++ centos6.sh
@@ -109,6 +109,8 @@ fi
 wget -O /etc/iptables.up.rules "https://raw.githubusercontent.com/kazmigithub/vpn/master/iptables.up.rules"
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
 sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.d/rc.local
+MYIP=`curl -s ifconfig.me`;
+MYIP2="s/xxxxxxxxx/$MYIP/g";
 sed -i $MYIP2 /etc/iptables.up.rules;
 iptables-restore < /etc/iptables.up.rules
 sysctl -w net.ipv4.ip_forward=1
@@ -119,13 +121,9 @@ cd
 
 # configure openvpn client config
 cd /etc/openvpn/
-wget -O /etc/openvpn/1194-client.ovpn "https://raw.githubusercontent.com/kazmigithub/vpn/master/1194-client.conf"
+wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/arieonline/autoscript/master/conf/1194-client.conf"
 sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
 PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
-useradd -M -s /bin/false kazmi
-echo "kazmi:$PASS" | chpasswd
-echo "username" > pass.txt
-echo "password" >> pass.txt
 tar cf client.tar 1194-client.ovpn pass.txt
 cp client.tar /home/vps/public_html/
 cd
